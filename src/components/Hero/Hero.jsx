fc3cc41acbafb8e00fd33d4ef79c124037c4e136
Mihai Hiticaș <hiticasmihai7@gmail.com>
2025-12-02 18:57:59 +0200

    add image to hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,11 @@
 import './Hero.scss'
+import imageOmelette from '../../assets/image-omelette.jpeg'
 
 function Hero() {
   return (
     <div className="hero">
-      <p className="text-large">Hero</p>
+      <img className="image-omelette" src={imageOmelette} alt="image" />
+      <p className="text-large">Heros</p>
       <p className="text-small">Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
     </div>
   )
