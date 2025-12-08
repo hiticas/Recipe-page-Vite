8fea72e0f476aea5f56efc92b8e91c0bcb9c6d15
Mihai Hiticas <hiticasmihai7@gmail.com>
2025-12-08 10:20:29 +0200

    style hero section mobile

--- src/components/Preparation/Preparation.jsx
+++ src/components/Preparation/Preparation.jsx
@@ -3,11 +3,11 @@ import './Preparation.scss'
 function Preparation() {
   return (
     <div className="preparation">
-      <p className="text-subtitle prep">Preparation</p>
+      <p className="text-subtitle prep-title">Preparation Time</p>
       <ul className="content">
-        <li className="item text-body">Total: Approximately 10 minutes</li>
-        <li className="item text-body">Preparation: 5 minutes</li>
-        <li className="item text-body">Cooking: 5 minutes</li>
+        <li className="item text-body"><strong>Total:</strong> Approximately 10 minutes</li>
+        <li className="item text-body"><strong>Preparation:</strong> 5 minutes</li>
+        <li className="item text-body"><strong>Cooking:</strong> 5 minutes</li>
       </ul>
     </div>
   )
