fd2fb61a0f62741241d7a04af0235f280a4dbadb
Mihai Hiticaș <hiticasmihai7@gmail.com>
2025-12-08 13:54:53 +0200

    update preparation section

--- src/components/Preparation/Preparation.jsx
+++ src/components/Preparation/Preparation.jsx
@@ -3,12 +3,14 @@ import './Preparation.scss'
 function Preparation() {
   return (
     <div className="preparation">
-      <p className="text-subtitle prep-title">Preparation Time</p>
-      <ul className="content">
-        <li className="item text-body"><strong>Total:</strong> Approximately 10 minutes</li>
-        <li className="item text-body"><strong>Preparation:</strong> 5 minutes</li>
-        <li className="item text-body"><strong>Cooking:</strong> 5 minutes</li>
-      </ul>
+      <div className="box">
+        <p className="text-subtitle prep-title">Preparation Time</p>
+        <ul className="content">
+          <li className="item text-body"><strong>Total:</strong> Approximately 10 minutes</li>
+          <li className="item text-body"><strong>Preparation:</strong> 5 minutes</li>
+          <li className="item text-body"><strong>Cooking:</strong> 5 minutes</li>
+        </ul>
+      </div>
     </div>
   )
 }
